0416a3c8375cc76aab1d91869fbb9675fb2ff3ff
Dheeraj Yarramsetty <dheeeraj0259@gmail.com>
2022-06-26 19:18:56 -0500

    Minor change to unit tests

--- block.test.js
+++ block.test.js
@@ -1,13 +1,15 @@
 const Block = require("./block");
 
 describe("Block", () => {
-    const timestamp = "test-date";
-    const lastHash = "test-hash";
-    const hash = "test-unique-hash";
-    const data = ["testdata1", "testdata2"];
-    const block = new Block({ timestamp, lastHash, hash, data });
-
     it("has a timestamp, lastHash, hash, and data property", () => {
+        // arrange
+        const timestamp = "test-date";
+        const lastHash = "test-hash";
+        const hash = "test-unique-hash";
+        const data = ["testdata1", "testdata2"];
+        // act
+        const block = new Block({ timestamp, lastHash, hash, data });
+        // assert
         expect(block.timestamp).toEqual(timestamp);
         expect(block.lastHash).toEqual(lastHash);
         expect(block.hash).toEqual(hash);
